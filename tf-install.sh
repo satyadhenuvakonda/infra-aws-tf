470879d51872bc991e1a0745a7f0d27236436c0a
satyadhenuvakonda <satya.dhenuvakonda@gmail.com>
2020-04-17 16:56:45 +0100

    fixing bugs

--- tf-install.sh
+++ tf-install.sh
@@ -3,6 +3,6 @@
 # install Terraform
 wget https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_amd64.zip -O /tmp/terraform.zip
 unzip /tmp/terraform.zip -d /tmp/
-mv /tmp/terraform /usr/bin/terraform
-chmod +x /usr/bin/terraform
-rm -rf /tmp/terraform.zip
+sudo mv /tmp/terraform /usr/bin/terraform
+sudo chmod +x /usr/bin/terraform
+sudo rm -rf /tmp/terraform.zip
