a68c72bff6dc8e01bb550f88374e98ad7195a7d6
satyadhenuvakonda <50108645+satyadhenuvakonda@users.noreply.github.com>
2020-04-18 16:11:00 +0100

    Automating terraform installation

--- tf-install.sh
+++ tf-install.sh
@@ -1,8 +1,31 @@
-#! /bin/bash
+#!/bin/bash
 
-# install Terraform
-wget https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_amd64.zip -O /tmp/terraform.zip
-unzip /tmp/terraform.zip -d /tmp/
-sudo mv /tmp/terraform /usr/bin/terraform
-sudo chmod +x /usr/bin/terraform
-sudo rm -rf /tmp/terraform.zip
+function terraform-install() {
+  [[ -f ${HOME}/bin/terraform ]] && echo "`${HOME}/bin/terraform version` already installed at ${HOME}/bin/terraform" && return 0
+  
+  # assign variable (LATEST_URL) 
+  LATEST_URL=$(curl -sL https://releases.hashicorp.com/terraform/index.json | jq -r '.versions[].builds[].url' | sort --version-sort | egrep -v 'rc|beta|alpha' | egrep 'linux.*amd64' |tail -1)
+
+  # pull from terraform and place in the folder (/tmp/terraform.zip)
+  curl ${LATEST_URL} > /tmp/terraform.zip
+
+  # make directory in the current user's bin folder.
+  mkdir -p ${HOME}/bin
+  
+  # change directory and unzip in the folder path 
+  (cd ${HOME}/bin && unzip /tmp/terraform.zip)
+  if [[ -z $(grep 'export PATH=${HOME}/bin:${PATH}' ~/.bashrc) ]]; then
+  	echo 'export PATH=${HOME}/bin:${PATH}' >> ~/.bashrc
+  fi
+  
+  echo "Installed: `${HOME}/bin/terraform version`"
+  
+  cat - << EOF 
+ 
+Run the following to reload your PATH with terraform:
+  source ~/.bashrc
+EOF
+}
+
+# calling the defined function above
+terraform-install
